b0d020244863caa1dfbc7d9be23cd94977a7142b
krthush <33790334+krthush@users.noreply.github.com>
2020-02-14 03:30:50 +0000

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,7 +8,7 @@
 
 		<p class="container" style="white-space: pre-line;">
 
-			A reddit style website for pain points (problems that are frequent and intense) and ideas for startups.
+			A reddit style website for pain points (problems that are frequent and intense) and solutions/ideas to help with founding a startup.
 
 			Mainly to be used by myself (Thush) and rest of the emit Team (Stephen & Pius), but anyone with burning problems and cool ideas feel free to post!
 
